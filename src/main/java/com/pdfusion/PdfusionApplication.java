0da1550f2cdb74c37b9f1adcbb93cebc6de5f418
cleberdasilva <cleber.supertux@gmail.com>
2026-07-10 15:09:56 -0300

    pdfusion - Add Maven build output directory to .gitignore

---
+++ src/main/java/com/pdfusion/PdfusionApplication.java
@@ -0,0 +1,13 @@
+package com.pdfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfusionApplication.class, args);
+	}
+
+}
